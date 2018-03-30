000dbcdcf2d087abc28013f14eaae564e4dc0af9
Koudjo AMETEPE <freelance28@pca.ma>
2018-03-30 22:32:12 +0100

    First Version of Mining and understanding BlockChain

--- koudjoCoin/Program.cs
+++ koudjoCoin/Program.cs
@@ -19,39 +19,42 @@ namespace koudjoCoin
             }
                 
             BlockChain koudjochain = new BlockChain(difficulty);
-            Console.WriteLine(" Adding Block ");
-            koudjochain.AddBlock(new Block(1, "15/2/2018", new CoinData {Amount = 14 }));
-            Console.WriteLine(" Adding Block ");
-            koudjochain.AddBlock(new Block(1, "17/2/2018", new CoinData { Amount = 20 }));
-            Console.WriteLine(" Adding Block ");
-            koudjochain.AddBlock(new Block(1, "18/2/2018", new CoinData { Amount = 41 }));
-            //Console.WriteLine(" Current Chain ");
-            //Console.WriteLine(JsonConvert.SerializeObject(koudjochain));
-            //Console.WriteLine("");
-            //Console.WriteLine(" Is chain valid ? " + koudjochain.IsValidChain());
+            koudjochain.CreateTransaction(new Transaction("john-doe-address", "john-smith-address", 100));
+            koudjochain.CreateTransaction(new Transaction("john-smith-address", "jane-doe-address", 50));
+            koudjochain.CreateTransaction(new Transaction("john-smith-address", "jane-doe-address", 70));
+
+            Console.WriteLine("Start mining ");
+            koudjochain.MinePendingTransactions("maverick-address");
+
+            Console.WriteLine(string.Format(" Balance for Maverick : {0}", koudjochain.GetBalanceForAddress("maverick-address")));
+
+            Console.WriteLine("Start mining again");
+            koudjochain.MinePendingTransactions("maverick-address");
+
+            Console.WriteLine(string.Format(" Balance for Maverick : {0}", koudjochain.GetBalanceForAddress("maverick-address")));
+            
         }
     }
 
+    #region Models 
     public class Block
     {
-        public int Index { get; set; }
-        public string Timestamp { get; set; }
-        public CoinData Data { get; set; }
+        public DateTime Timestamp { get; set; }
+        public List<Transaction> Transactions { get; set; }
         public string Hash { get; set; }
         public string PreviousHash { get; set; }
         public int Nonce { get; set; }
-        public Block(int _index, string _timestamp, CoinData _data, string _previoushash = "")
-        {
-            this.Index = _index;
+        public Block(DateTime _timestamp, List<Transaction> _transactions, string _previoushash = "")
+        {            
             this.Timestamp = _timestamp;
-            this.Data = _data;
+            this.Transactions = _transactions;
             this.Hash = CalculateHash();
             this.PreviousHash = _previoushash;
         }
 
         public string CalculateHash()
         {
-            string data = this.Index + this.Timestamp + this.Data.ToString() +  this.Nonce.ToString();
+            string data = string.Concat(this.PreviousHash, this.Timestamp.ToLongDateString(), Transactions, this.Nonce.ToString());
             var sha256Provider = HashAlgorithm.Create("SHA256");
             var binHash = sha256Provider.ComputeHash(Encoding.ASCII.GetBytes(data));
             return (BitConverter.ToString(binHash)).Replace("-", "");
@@ -63,23 +66,38 @@ namespace koudjoCoin
                 this.Nonce++;
                 this.Hash = this.CalculateHash();
             }
-            Console.WriteLine(" Mining finished " + this.Hash);
+            Console.WriteLine(" Block mined " + this.Hash);
         }
     }
 
-    public class CoinData
+    public class Transaction
     {
         public int Amount { get; set; }
+        public string FromAddress { get; set; }
+        public string ToAddress { get; set; }
+
+        public Transaction(string _fromAddress, string _toAddress, int _amount)
+        {
+            this.FromAddress = _fromAddress;
+            this.ToAddress = _toAddress;
+            this.Amount = _amount;
+        }
         public override string ToString()
         {
-            return Amount.ToString();
+            return string.Concat(FromAddress, ToAddress, Amount.ToString());
         }
     }
+    #endregion
+
+    #region BlockChain
     public class BlockChain
     {
         public List<Block> Chain {get; set; }
 
         public int Difficulty { get; set; }
+
+        public List<Transaction> PendingTransactions { get; set; }
+        public int MiningReward { get; set; }
 
         public BlockChain()
         {
@@ -95,21 +113,56 @@ namespace koudjoCoin
         private void Init()
         {
             Difficulty = 2;
+            MiningReward = 100;
+            PendingTransactions = new List<Transaction>();
             Chain = new List<Block>();
             this.Chain.Add(CreateGenesisBlock());
         }
-
-        public void AddBlock(Block newBlock)
-        {           
-            newBlock.PreviousHash = this.Chain.Last().Hash;
-            newBlock.MineBlock(this.Difficulty);
-            this.Chain.Add(newBlock);
-        }
+ 
         public Block CreateGenesisBlock()
         {
-            return new Block(0, "10/03/2018", new CoinData { Amount = 4}, "0");
+            List<Transaction> beginning = new List<Transaction>();
+            beginning.Add(new Transaction("Epoch", "Universe", 4));
+
+            return new Block(new DateTime(), beginning, "0");
         }
 
+        public void MinePendingTransactions(string miningRewardAddress)
+        {
+            Block block = new Block(DateTime.Now, this.PendingTransactions);
+            block.MineBlock(this.Difficulty);
+
+            Console.WriteLine(" Block sucessfully mined ");
+            this.Chain.Add(block);
+
+            this.PendingTransactions = new List<Transaction> { new Transaction(null, miningRewardAddress, this.MiningReward) };
+        }
+
+        public void CreateTransaction(Transaction transaction)
+        {
+            this.PendingTransactions.Add(transaction);
+        }
+        public int GetBalanceForAddress(string address)
+        {
+            int balance = 0;
+            foreach (Block block in Chain)
+            {
+                foreach (Transaction transaction in block.Transactions)
+                {
+                    if(string.Compare(transaction.FromAddress, address) == 0)
+                    {
+                        balance = balance - transaction.Amount;
+                    }
+
+                    if (string.Compare(transaction.ToAddress, address) == 0)
+                    {
+                        balance = balance + transaction.Amount;
+                    }
+                }
+            }
+
+            return balance;
+        }
         public bool IsValidChain()
         {
             Block[] arBlocks = this.Chain.ToArray();
@@ -133,5 +186,6 @@ namespace koudjoCoin
             return true;
         }
     }
+    #endregion
 
 }
